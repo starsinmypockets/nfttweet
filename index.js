1ef8ea1856201e7d49ea2070bef806f29accf992
Paul Walker <starsinmypockets@gmail.com>
2021-05-03 12:30:40 -0400

    Add twitter image upload

--- index.js
+++ index.js
@@ -17,8 +17,10 @@ const {
 const CONTRACT_EVENTS_ARRAY = CONTRACT_EVENTS.split(',')
 const Web3 = require('web3')
 const Twitter = require('twitter')
-const restClient = require('node-rest-client-promise').Client();
+const restClient = require('node-rest-client-promise').Client()
+const axios = require('axios')
 const web3 = new Web3(new Web3.providers.WebsocketProvider(WSURL))
+
 const twitterClient = new Twitter({
   consumer_key: TWITTER_API_KEY,
   consumer_secret: TWITTER_API_SECRET_KEY,
@@ -26,11 +28,33 @@ const twitterClient = new Twitter({
   access_token_secret: TWITTER_ACCESS_TOKEN_SECRET
 })
 
-function postToTwitter(event) {
-  const msg = eval('`'+ TWITTER_MESSAGE_TEMPLATE + '`')
-  return twitterClient.post('statuses/update', {status: msg},  function(error, tweet, response) {
+async function postToTwitter(event) {
+  const msg = eval('`'+ TWITTER_MESSAGE_TEMPLATE + '`') // can replace this with message template
+  let opts = { status: msg }
+
+  // If you always use an image you can use `if (true)` here or remove conditional
+  if (TWITTER_MESSAGE_TEMPLATE.indexOf('metadata.data.image') >= 0) {
+    const img_url = event.metadata.data.image
+    
+    // get png binary data
+		const result = await axios.request({
+			responseType: 'arraybuffer',
+			url: img_url,
+			method: 'get',
+			headers: {
+        'Content-Type': 'image/png', // if the image type changes this too
+			},
+    })
+
+    const data = result.data
+    const uploadResult = await twitterClient.post('media/upload', { media: data})
+    const media_id = uploadResult.media_id
+    opts = { status: msg, media_ids: media_id }
+  }
+
+  return twitterClient.post('statuses/update', opts, function(error, tweet, response) {
       if (error) return console.log(JSON.stringify(error))
-  });
+  })
 }
 
 async function getContractAbi() {
